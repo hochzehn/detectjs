73a9eaa7075148eb6d55dfd952090745eae67256
janpapenbrock <j.papenbrock@hochzehn.com>
2016-05-27 11:25:20 +0200

    Add dependencies to this repository to have them available for automated Dockerhub build.

--- bin/updateLibraries.sh
+++ bin/updateLibraries.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TARGET_DIR=lib/johnmichel/Library-Detector-for-Chrome
+TARGET_DIR=lib/vendor/johnmichel/Library-Detector-for-Chrome
 TARGET_FILE="$TARGET_DIR"/libraries.js
 TEMP_FILE="$TARGET_DIR"/libraries.original.js
 
@@ -10,12 +10,13 @@ mkdir -p "$TARGET_DIR"
 # Full credit to https://github.com/johnmichel/Library-Detector-for-Chrome/
 wget -O "$TEMP_FILE" https://raw.githubusercontent.com/johnmichel/Library-Detector-for-Chrome/master/library/libraries.js
 
-
 # Prepare the libraries file.
-## 0. Copy downloaded file.
-cp "$TEMP_FILE" "$TARGET_FILE"
-## 1. Remove parameter "win" for the test functions and set it inside instead.
+## 1. Create source notice.
+echo "// Source: https://github.com/johnmichel/Library-Detector-for-Chrome (MIT License)" > "$TARGET_FILE"
+## 2. Copy downloaded file.
+cat "$TEMP_FILE" >> "$TARGET_FILE"
+## 3. Remove parameter "win" for the test functions and set it inside instead.
 sed -i 's/test:.*/test: function() {\n            var win = window;/g' $TARGET_FILE
-## 2. Export named variable as module.exports to have it available.
-VAR_NAME=$(head -n 1 $TARGET_FILE | sed -e 's/var //' | sed -e 's/ = {//')
+## 4. Export named variable as module.exports to have it available.
+VAR_NAME=$(head -n 1 $TEMP_FILE | sed -e 's/var //' | sed -e 's/ = {//')
 echo "\nmodule.exports = $VAR_NAME;" >> $TARGET_FILE
